458905e62673a7887934ef9147ac8b70cc4a6a94
Gianni Doria <3026648+gidor@users.noreply.github.com>
2019-05-14 10:47:11 +0200

    Add files via upload

---
+++ sql/shp_pozzi.sql
@@ -0,0 +1,16 @@
+SELECT 
+    X, Y, 
+    'C070202' as COD_CLASSE,
+    0 as FILE_ID,
+    ISTAT_ID as COM_ISTAT,
+    `DATA` as RILIEVO,
+     null as P_BORN,
+    MAT_SUP as P_MAT,
+    '91' as P_STA,
+    Z as P_QUO,
+    POS_SUP as P_POS,
+    '91' as P_UTE,
+    '91' as  P_F_TY,
+     '91' as P_F_REC
+FROM POZZI;
+ 
